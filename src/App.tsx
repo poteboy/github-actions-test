5f4f7e95fa24a9fc26fa8f998133d98c96cec820
poteboy <poteboy@poteboy.local>
2024-05-09 14:25:27 +0900

    hello;

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Box, k } from "@kuma-ui/core";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <k.div color="red" bg="white">
-      hello
-      <k.div color="blue" fontSize={24}>
-        world
-      </k.div>
-    </k.div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
